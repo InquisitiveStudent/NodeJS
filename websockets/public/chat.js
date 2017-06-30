faa156ecf3e8d30a5073436284738a25bbdd2507
InquisitiveStudent <lolaplolap1@hotmail.com>
2017-06-30 14:36:42 +0800

    Websocket (web chat)
    Using NodeJS(socket.io) and express to communicate between server and
    clients

---
+++ websockets/public/chat.js
@@ -0,0 +1,39 @@
+//Make connection
+
+var socket=io.connect('http://localhost:4000');
+
+
+//Query DOM
+
+var message= document.getElementById('message');
+var handle= document.getElementById('handle');
+var btn= document.getElementById('send');
+var output= document.getElementById('output');
+var feedback = document.getElementById('feedback')
+
+
+//Emit Events
+
+btn.addEventListener('click',function(){
+  //emit('name of the message', 'What is the message')
+
+  socket.emit('chat',{
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+
+message.addEventListener("keypress",function(){
+  socket.emit('typing', handle.value);
+});
+
+//Listen for Events
+socket.on('chat',function(data){
+    output.innerHTML +="<p><strong>"+data.handle+": </strong>"+data.message+"</p>"
+
+
+});
+socket.on('typing',function(data){
+    feedback.innerHTML = '<p><em>'+ data + " is typing a message</em></p>"
+});
